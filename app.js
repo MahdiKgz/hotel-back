1a2aa88bb71c20bd46419d6e1102a1f04419c4f7
mahdi.khoshghadam <mahdikgz7@gmail.com>
2026-03-17 02:07:58 +0330

    fix: cors error fix

--- app.js
+++ app.js
@@ -2,12 +2,15 @@ const express = require("express");
 const app = express();
 const morgan = require("morgan");
 const path = require("path");
+const cors = require("cors");
 
 app.use(express.json({ limit: "30mb" }));
 app.use(express.urlencoded({ limit: "30mb", extended: true }));
 app.use("/uploads", express.static(path.join(__dirname, "uploads/")));
 
 app.use(morgan("dev"));
+
+app.use(cors());
 
 const authRouter = require("./routers/v1/auth.router");
 const hotelRouter = require("./routers/v1/hotel.router");
