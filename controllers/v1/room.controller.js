40ed65d02ef58523396feb70e1fab6cb9d8ff142
mahdi.khoshghadam <mahdikgz7@gmail.com>
2026-03-24 23:07:37 +0330

    chore: crud on room

--- controllers/v1/room.controller.js
+++ controllers/v1/room.controller.js
@@ -1,3 +1,4 @@
+const Hotel = require("../../models/v1/Hotel.model");
 const Room = require("../../models/v1/Room.model");
 const { errorResponse, successResponse } = require("../../utils/responses");
 const { createRoomValidator } = require("../../validators/room.validator");
@@ -20,6 +21,71 @@ exports.create = async (req, res, next) => {
 
     await Room.create(req.body);
     return successResponse(res, 201, "room created successfully.");
+  } catch (err) {
+    next(err);
+  }
+};
+
+exports.remove = async (req, res, next) => {
+  try {
+    const { slug } = req.params;
+    const room = await Room.findOne({
+      where: {
+        slug,
+      },
+    });
+
+    if (room === null) {
+      return errorResponse(res, 400, "room NOT found !!");
+    }
+
+    await room.destroy({ where: { slug } });
+
+    return successResponse(res, 200, "room deleted successfully!!");
+  } catch (err) {
+    next(err);
+  }
+};
+
+exports.update = async (req, res, next) => {
+  try {
+    const { slug } = req.params;
+
+    await createRoomValidator.validate(req.body, { abortEarly: false });
+    const room = await Room.findOne({
+      where: { slug },
+    });
+
+    if (room === null) {
+      return errorResponse(res, 400, "room NOT found !!");
+    }
+
+    await room.update({ ...req.body }, { where: { slug } });
+    return successResponse(res, 200, "Room updated successfully !!");
+  } catch (err) {
+    next(err);
+  }
+};
+
+exports.getOne = async (req, res, next) => {
+  try {
+    const { slug } = req.params;
+    const room = await Room.findOne({
+      where: {
+        slug,
+      },
+      include: {
+        model: Hotel,
+        attributes: ["id", "name"],
+        as: "hotel",
+      },
+    });
+
+    if (room === null) {
+      return errorResponse(res, 404, "room NOT found !!");
+    }
+
+    return successResponse(res, 200, "", { room });
   } catch (err) {
     next(err);
   }
--- routers/v1/room.router.js
+++ routers/v1/room.router.js
@@ -1,9 +1,19 @@
 const express = require("express");
 const roomRouter = express.Router();
 const { roleGuard } = require("../../middlewares/roleGuard");
-const { create } = require("../../controllers/v1/room.controller");
+const {
+  create,
+  update,
+  getOne,
+  remove,
+} = require("../../controllers/v1/room.controller");
 const auth = require("../../middlewares/auth");
 
 roomRouter.route("/").post(auth, roleGuard("ADMIN,MANAGER"), create);
+roomRouter
+  .route("/:slug")
+  .delete(auth, roleGuard("ADMIN,MANAGER"), remove)
+  .put(auth, roleGuard("ADMIN,MANAGER"), update)
+  .get(auth, roleGuard("ADMIN,MANAGER"), getOne);
 
 module.exports = roomRouter;
